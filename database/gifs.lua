5ac6387dc225d1dd5efc8c8df5157d16864ded75
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-12 20:41:07 +0000

    Update gifs.lua - Added 1 new entries

--- database/gifs.lua
+++ database/gifs.lua
@@ -1,3 +1,7 @@
 -- FiveManage Gifs
 return {
+    ["mist_intro"] = {
+        label = "mist_intro",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/mist_intro.gif"
+    }
 }

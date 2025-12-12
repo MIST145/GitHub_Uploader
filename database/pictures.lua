022d46aac4263d06e9e91e16a5fc850602ef4ae8
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-12 20:41:07 +0000

    Update pictures.lua - Added 79 new entries

--- database/pictures.lua
+++ database/pictures.lua
@@ -1,3 +1,319 @@
 -- FiveManage Images
 return {
+    ["github_manager_banner"] = {
+        label = "github_manager_banner",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/github_manager_banner.png"
+    },
+    ["advanced_fertilizer"] = {
+        label = "advanced_fertilizer",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/advanced_fertilizer.png"
+    },
+    ["advancedrepairkit"] = {
+        label = "advancedrepairkit",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/advancedrepairkit.png"
+    },
+    ["advancedrepairkit2"] = {
+        label = "advancedrepairkit2",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/advancedrepairkit2.png"
+    },
+    ["ammo-firework"] = {
+        label = "ammo-firework",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/ammo-firework.png"
+    },
+    ["backpack"] = {
+        label = "backpack",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/backpack.png"
+    },
+    ["baggie"] = {
+        label = "baggie",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/baggie.png"
+    },
+    ["baggie_1oz"] = {
+        label = "baggie_1oz",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/baggie_1oz.png"
+    },
+    ["baggie_ak"] = {
+        label = "baggie_ak",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/baggie_ak.png"
+    },
+    ["baggie_coke"] = {
+        label = "baggie_coke",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/baggie_coke.png"
+    },
+    ["baggie_ogk"] = {
+        label = "baggie_ogk",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/baggie_ogk.png"
+    },
+    ["baggie_ph"] = {
+        label = "baggie_ph",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/baggie_ph.png"
+    },
+    ["baggie_sk"] = {
+        label = "baggie_sk",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/baggie_sk.png"
+    },
+    ["baggie_ww"] = {
+        label = "baggie_ww",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/baggie_ww.png"
+    },
+    ["box_ammo_firework"] = {
+        label = "box_ammo_firework",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/box_ammo_firework.png"
+    },
+    ["brick_coke"] = {
+        label = "brick_coke",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/brick_coke.png"
+    },
+    ["car-battery"] = {
+        label = "car-battery",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/car-battery.png"
+    },
+    ["car-door"] = {
+        label = "car-door",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/car-door.png"
+    },
+    ["car-hood"] = {
+        label = "car-hood",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/car-hood.png"
+    },
+    ["car-radio"] = {
+        label = "car-radio",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/car-radio.png"
+    },
+    ["car-rim"] = {
+        label = "car-rim",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/car-rim.png"
+    },
+    ["car-seat"] = {
+        label = "car-seat",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/car-seat.png"
+    },
+    ["car-speakers"] = {
+        label = "car-speakers",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/car-speakers.png"
+    },
+    ["car-tire"] = {
+        label = "car-tire",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/car-tire.png"
+    },
+    ["car-trunk"] = {
+        label = "car-trunk",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/car-trunk.png"
+    },
+    ["cleaningkit"] = {
+        label = "cleaningkit",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/cleaningkit.png"
+    },
+    ["coca"] = {
+        label = "coca",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/coca.png"
+    },
+    ["coca_seed"] = {
+        label = "coca_seed",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/coca_seed.png"
+    },
+    ["cocaine"] = {
+        label = "cocaine",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/cocaine.png"
+    },
+    ["cocaine_processing_table"] = {
+        label = "cocaine_processing_table",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/cocaine_processing_table.png"
+    },
+    ["deluxemoneywash"] = {
+        label = "deluxemoneywash",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/deluxemoneywash.png"
+    },
+    ["drive"] = {
+        label = "drive",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/drive.png"
+    },
+    ["drive_bike"] = {
+        label = "drive_bike",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/drive_bike.png"
+    },
+    ["drive_boat"] = {
+        label = "drive_boat",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/drive_boat.png"
+    },
+    ["drive_truck"] = {
+        label = "drive_truck",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/drive_truck.png"
+    },
+    ["drug_scales_high"] = {
+        label = "drug_scales_high",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/drug_scales_high.png"
+    },
+    ["drug_scales_low"] = {
+        label = "drug_scales_low",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/drug_scales_low.png"
+    },
+    ["drugscales"] = {
+        label = "drugscales",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/drugscales.png"
+    },
+    ["empty_jar"] = {
+        label = "empty_jar",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/empty_jar.png"
+    },
+    ["fertilizer"] = {
+        label = "fertilizer",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/fertilizer.png"
+    },
+    ["garden_shovel"] = {
+        label = "garden_shovel",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/garden_shovel.png"
+    },
+    ["hacking_device_atm"] = {
+        label = "hacking_device_atm",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/hacking_device_atm.png"
+    },
+    ["id_card"] = {
+        label = "id_card",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/id_card.png"
+    },
+    ["joint_ak"] = {
+        label = "joint_ak",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/joint_ak.png"
+    },
+    ["joint_ogk"] = {
+        label = "joint_ogk",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/joint_ogk.png"
+    },
+    ["joint_ph"] = {
+        label = "joint_ph",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/joint_ph.png"
+    },
+    ["joint_roller"] = {
+        label = "joint_roller",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/joint_roller.png"
+    },
+    ["joint_sk"] = {
+        label = "joint_sk",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/joint_sk.png"
+    },
+    ["joint_ww"] = {
+        label = "joint_ww",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/joint_ww.png"
+    },
+    ["liquid_fertilizer"] = {
+        label = "liquid_fertilizer",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/liquid_fertilizer.png"
+    },
+    ["lockpick"] = {
+        label = "lockpick",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/lockpick.png"
+    },
+    ["lockpick2"] = {
+        label = "lockpick2",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/lockpick2.png"
+    },
+    ["lockpick_reg"] = {
+        label = "lockpick_reg",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/lockpick_reg.png"
+    },
+    ["lockpick_safe"] = {
+        label = "lockpick_safe",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/lockpick_safe.png"
+    },
+    ["methtable"] = {
+        label = "methtable",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/methtable.png"
+    },
+    ["nitrous"] = {
+        label = "nitrous",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/nitrous.png"
+    },
+    ["og-kush-oz"] = {
+        label = "og-kush-oz",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/og-kush-oz.png"
+    },
+    ["oldmoneywash"] = {
+        label = "oldmoneywash",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/oldmoneywash.png"
+    },
+    ["ozjar_ak"] = {
+        label = "ozjar_ak",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/ozjar_ak.png"
+    },
+    ["ozjar_ogk"] = {
+        label = "ozjar_ogk",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/ozjar_ogk.png"
+    },
+    ["ozjar_ph"] = {
+        label = "ozjar_ph",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/ozjar_ph.png"
+    },
+    ["ozjar_sk"] = {
+        label = "ozjar_sk",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/ozjar_sk.png"
+    },
+    ["ozjar_ww"] = {
+        label = "ozjar_ww",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/ozjar_ww.png"
+    },
+    ["plane_license"] = {
+        label = "plane_license",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/plane_license.png"
+    },
+    ["plant_cutters"] = {
+        label = "plant_cutters",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/plant_cutters.png"
+    },
+    ["plant_pot"] = {
+        label = "plant_pot",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/plant_pot.png"
+    },
+    ["potting_mix"] = {
+        label = "potting_mix",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/potting_mix.png"
+    },
+    ["repairkit"] = {
+        label = "repairkit",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/repairkit.png"
+    },
+    ["repairkit2"] = {
+        label = "repairkit2",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/repairkit2.png"
+    },
+    ["rollpaper"] = {
+        label = "rollpaper",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/rollpaper.png"
+    },
+    ["rollpaper2"] = {
+        label = "rollpaper2",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/rollpaper2.png"
+    },
+    ["rollpapers"] = {
+        label = "rollpapers",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/rollpapers.png"
+    },
+    ["scrapelectronics"] = {
+        label = "scrapelectronics",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/scrapelectronics.png"
+    },
+    ["scrapglass"] = {
+        label = "scrapglass",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/scrapglass.png"
+    },
+    ["scrapmetal"] = {
+        label = "scrapmetal",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/scrapmetal.png"
+    },
+    ["scrapplastic"] = {
+        label = "scrapplastic",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/scrapplastic.png"
+    },
+    ["scraprubber"] = {
+        label = "scraprubber",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/scraprubber.png"
+    },
+    ["scrapsteel"] = {
+        label = "scrapsteel",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/scrapsteel.png"
+    },
+    ["scuba_fins"] = {
+        label = "scuba_fins",
+        image = "https://raw.githubusercontent.com/MIST145/GitHub_Uploader/main/assets/inventory/scuba_fins.png"
+    }
 }
